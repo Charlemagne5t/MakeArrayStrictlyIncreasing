ef05b0ae19e7443bfd93e5a88cf951f2495e8641
alek <wait82985@gmail.com>
2023-06-17 13:29:36 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,16 +1,45 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeSet;
 
 public class Solution {
     public int makeArrayIncreasing(int[] arr1, int[] arr2) {
-
-        Arrays.sort(arr2);
-        return dfs(arr1, arr2, 1, 0);
-    }
-    private int dfs(int[] arr1, int[] arr2, int i, int j){
-        if(i == arr1.length ){
-            return -1;
+        int n = arr1.length;
+        TreeSet<Integer> sortedArr2 = new TreeSet<>();
+        for (int num : arr2) {
+            sortedArr2.add(num);
         }
-        int result = 0;
-        if(arr1[i] < arr1[i -1])
+        Map<Integer, Integer> dp = new HashMap<>();
+        dp.put(-1, 0);
+
+        for (int i = 0; i < n; i++) {
+            Map<Integer, Integer> temp = new HashMap<>();
+            for (Map.Entry<Integer, Integer> entry : dp.entrySet()) {
+                int num = entry.getKey();
+                int cost = entry.getValue();
+
+                if (arr1[i] > num) {
+                    temp.put(arr1[i], Math.min(temp.getOrDefault(arr1[i], Integer.MAX_VALUE), cost));
+                }
+
+                Integer higherNum = sortedArr2.higher(num);
+                if (higherNum != null) {
+                    temp.put(higherNum, Math.min(temp.getOrDefault(higherNum, Integer.MAX_VALUE), cost + 1));
+                }
+            }
+            dp = temp;
+
+            if (dp.isEmpty()) {
+                return -1;
+            }
+        }
+
+        int minCost = Integer.MAX_VALUE;
+        for (int cost : dp.values()) {
+            minCost = Math.min(minCost, cost);
+        }
+
+        return minCost;
     }
 }
